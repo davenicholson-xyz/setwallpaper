04fcd90b49d799e3a8352297942a84457c5231ec
Dave Nicholson <me@davenicholson.xyz>
2024-10-07 08:34:59 +0100

    added docs@

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
-#[cfg(target_family = "unix")]
+//! This module provides functionality to manage system wallpapers.
+//!
+//! You can use the functions in this module to set wallpapers on different platforms.#[cfg(target_family = "unix")]
 pub mod unix;
 
 #[cfg(target_family = "windows")]
 pub mod windows;
 
+/// This function sets the wallpaper from the given file path.
+///
+/// # Arguments
+///
+/// * `file_path` - A string slice that holds the file path of the wallpaper.
+///
+/// # Example
+///
+/// ```
+/// let result = set_wallpaper("/path/to/wallpaper.png");
+/// assert!(result.is_ok());
+/// ```
 pub fn from_file(filepath: &str) -> Result<(), std::io::Error> {
     #[cfg(target_family = "unix")]
     unix::set_wallpaper(filepath)?;
